fdefcf6073d07ed52c4a6ec6fd6cce5961cf411f
Raul Molina <raul.molina@usalesiana.edu.bo>
2025-10-02 09:22:20 -0400

    creacion workflow build y test

---
+++ tests/test_app.py
@@ -0,0 +1,21 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    res = client.get("/")
+    assert res.status_code == 200
+    assert res.get_json() == {"message": "Hola, mundo desde Flask con GitHub Actions!"}
+    
+def test_secret(client):
+    res = client.get("/secret")
+    assert res.status_code == 200
+    print(res.get_json())
+    assert res.get_json() == {"secret": os.getenv("MY_SECRET", "No se encontró el secret")}
